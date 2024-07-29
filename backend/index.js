28d045bcd9422ced6e394d96b276d1c6208e6fcb
Sergio Peixoto <sergiopeixoto3@gmail.com>
2024-07-29 17:06:20 +0100

    added cors

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,29 @@ const app = express()
 
 const port = process.env.PORT || 3002
 
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+
+app.use(function (req, res, next) {
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, Content-Type, Authorization"
+  );
+
+  // Pass to next layer of middleware
+  next();
+});
+
+// Cross Origin Resource Sharing
+app.use(cors(corsOptions));
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
